9a20420f8560668991c33679835681ac74074b28
David.Johnson <davidt.johnson75@yahoo.com>
2019-11-05 19:27:23 -0600

    Get by ID working

--- server/server.js
+++ server/server.js
@@ -8,13 +8,29 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.get("/api", (req, res) => {
+server.get("/", (req, res) => {
     db("cars")
       .then(cars => res.status(200).json(cars))
   
       .catch(error =>
         res.status(500).json({
           Error: "Your attempt to retrieve the vehicles has failed."
+        })
+      );
+  });
+
+  server.get("/api/:id", (req, res) => {
+      const id = req.params.id;
+    db("cars")
+    .where({id})
+    .first()
+    .then(car => {
+        res.status(200).json(car);
+    })
+  
+      .catch(error =>
+        res.status(500).json({
+          Error: "That vehicle was not found."
         })
       );
   });
